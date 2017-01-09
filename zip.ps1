0fe752d71d7464e9cde908a92be82f218e210c67
ashish7888 <ashish7888@gmail.com>
2017-01-10 01:27:23 +0530

    Version 1.01

---
+++ zip.ps1
@@ -0,0 +1,16 @@
+﻿Function ZipEveryThing($src, $dest)
+{
+
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+$zip = Get-ChildItem $src -Filter *.zip
+
+
+Foreach ($zi IN $zip)
+{
+$all = $src + $zi
+[System.IO.Compression.ZipFile]::ExtractToDirectory($all,$dest)
+}
+
+}
+ZipEveryThing -src "C:\Users\ashish-pc\Documents\Zip\" -dest "C:\Users\ashish-pc\Documents\Zip\UnZip\"
